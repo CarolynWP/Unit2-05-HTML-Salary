b2c216597ed2490b5b96571c41de3c210f8ceb22
Carolyn Webster-Pless <99274537+CarolynWP@users.noreply.github.com>
2022-03-29 14:53:47 +0000

    stuff be not working

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function salary () {
+	let hours = parseInt(document.getElementById('hours').value);
+	let rate = parseInt(document.getElementById('rate').value);
+	const TAX_RATE = 0.1805;
+
+	let total = hours * rate;
+	let tax = total * TAX_RATE;
+	let pay = total - tax;
+
+	document.getElementById('pay').innerHTML = 'your pay will be' + pay +
+}
